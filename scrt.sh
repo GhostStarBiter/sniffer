c01d5b5cfd770bb427b1ae232961911925f82ed5
Dmitriy <dm.zelenets@gmail.com>
2017-02-02 16:16:18 +0200

    Update scrt.sh

--- scrt.sh
+++ scrt.sh
@@ -5,20 +5,15 @@ $SNIF=program
 echo -e "Enter type of net connection: eth or wlan?\n"
 read $conn_type
 ##
-if [ "$conn_type" = "eth" ]
-then echo -e "Enter ip address: \n"
-read $ip
-elif [ "$conn_type" = "wlan" ]
-then echo -e "Enter ip address: \n"
-read $ip
-elif [ -z "$conn_type" ]
-then 
-echo "Connection type not entered. Quit."
-exit 1
-fi
+case "$conn_type" in
+  eth | wlan ) read -p "Enter ip adress: " $ip;;
+  * ) echo "Entered not correct type of net connection!"
+      exit 1;;
+ecas
 
-if [ -n $ip ]
+if [ -n $ip | "$conn_type"="eth" | "$conn_type"="wlan" ]
 then $SNIF $conn_type $ip
+elif exit 1
 fi
 
 exit 0
