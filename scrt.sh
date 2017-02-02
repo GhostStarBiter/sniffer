8f0b0135c35d85e3fabd620e4f7c2c98d7a0b6b2
Dmitriy <dm.zelenets@gmail.com>
2017-02-03 00:21:03 +0200

    Update scrt.sh

--- scrt.sh
+++ scrt.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-$PATH=$PATH.
-$SNIF=program
+$PATH=$PATH:.
+$SNIF=cat
 
-echo -e "Enter type of net connection: eth or wlan?\n"
+echo -e "Enter type of net connection: eth wlan?\n or type --help for helpscreen"
 read $conn_type
 ##
 case "$conn_type" in
-  eth | wlan ) read -p "Enter ip adress: " $ip;;
-  * ) echo "Entered not correct type of net connection!"
+  [-] [-] [hH] [eE] [lL] [pP] ) echo "Screening helpscreen";;
+  [eE] [tT] [hH] | [wW] [lL] [aA] [nN] ) read -p "Enter ip adress: " $ip;;
+  * ) echo "Entered not correct type of net connection! Type --help for screening help"
       exit 1;;
 ecas
 
-if [ -n $ip | "$conn_type"="eth" | "$conn_type"="wlan" ]
-then $SNIF $conn_type $ip
-elif exit 1
-fi
+case "$ip" in
+  [0-9][0-9][0-9].[0-9][0-9][0-9].[0-9] ) echo "$SNIF $conn_type $ip"
+  * ) echo "Entered not correct ip adress! " 
+      exit 1 ;;
+ecas 
+
 
 exit 0
