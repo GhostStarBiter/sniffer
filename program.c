f4ae3f6d6ecc833580dc1c50ce1a334fa8bcbd10
Dmitriy <dm.zelenets@gmail.com>
2017-02-07 17:31:39 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>		// malloc(..) func	
-#include <stdio.h> 		// fopen(..) func
+#include <stdio.h> 		// fopen(..) func, fprintf(..) func
 #include <time.h>		// time func's
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,18 +7,34 @@
 #include <unistd.h>
 #include <netinet/in.h>   //sockaddr_in (internet socket address)
 
+
+void sniffPacket(unsigned char *, int );
 FILE *logfile;
-int daemon_socket;
+int dae_sock;
 int length;
-struct sockaddr_in source_addr, dest_addr; // declaration of the internet socket
-
-
+struct sockaddr_in source_addr, dest_addr;
+char *get_time(){
+		time_t rawtime;
+		struct tm *tm_info;
+		time( &rawtime );
+		tm_info = localtime( &rawtime );
+		curr_time = asctime( tm_info );
+		return( &curr_time );
+}
 
 
 
 int main(int argc, char *argv[]) // argv hold (1)the type of the net [eth, wlan], (2)N of the net type[0,1,2,..etc],(3) ip address
 {
-  if(argc<2) return 1;
-  
-  daemon_socket = socket(AF_INET,SOCK_STREAM(????)  ,  );
+	if( argc < 2 ){ printf(%s\n,"Program get no arguments!"); return 1; }; // if program launched without parameters
+  if(argc == 2 && argv[argc] == "start"){ 
+/*=begin=*/
+	logfile = fopen(daelog.txt,"a+");
+  if(logfile==NULL){printf(%s\n, "Unable to add records to file daelog.txt. If file not exist you need to create it @home_dir.");};
+	char *moment[80];
+	moment = get_time();
+	fprintf(logfile,"Sniffer started %s\t\n ",moment);
+	/*CREATE BLANK SOCKET*/
+  dae_sock = socket(AF_INET,SOCK_STREAM(????)  ,  );
+	if(dae_sock < 0){printf};
 }
